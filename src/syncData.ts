e63b8caeab9582d25da999f83d4c11b67798642a
abushamacd <abushamacd@gmail.com>
2025-01-18 13:24:54 +0600

    update

--- src/syncData.ts
+++ src/syncData.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 import { asyncForEach } from './utilities/asyncForEach'
 import { connectDatabases } from './utilities/bootStrap'
 import { errorLogger } from './utilities/logger'
-import { localPrisma } from './utilities/prisma'
+import { localPrisma, remotePrisma } from './utilities/prisma'
 
 export async function syncData() {
   try {
@@ -16,11 +17,22 @@ export async function syncData() {
         where: { isSynced: false },
       })
 
-      await asyncForEach(unsyncedData, async (product: any) => {
-        console.log(product)
-      })
+      if (unsyncedData.length > 0) {
+        await asyncForEach(unsyncedData, async (data: any) => {
+          const result = await remotePrisma.user.create({ data: data })
+          // Mark as synced in the local database
+          if (result) {
+            await localPrisma.user.update({
+              where: { id: data?.id },
+              data: { isSynced: true },
+            })
+          }
+        })
+      } else {
+        console.log(`No new entry`)
+      }
     } catch (error) {
-      errorLogger.error('Error syncing invoices:', error)
+      errorLogger.error('Error syncing data:', error)
     }
   } catch (err) {
     console.error('❌ Error during database connection:', err)
